a4c4ffac83700a5f9ebadd18d48f83ea426f9768
LeniSetiarini09 <leni.setiarini0903@gmail.com>
2022-10-03 14:14:14 +0700

    mengubah judul

--- pw/index.php
+++ pw/index.php
@@ -23,7 +23,7 @@ $buku = query("SELECT * FROM buku");
     <div class="container ">
         <div class="card mt-5 bg-dark bg-gradient text-light">
             <div class="card-body text-light">
-                <h1 class="display-3 text-center fw-bold ">Daftar Buku</h1>
+                <h1 class="display-3 text-center fw-bold ">Daftar Jenis Buku</h1>
                 <a href="tambah.php">Tambah Data Mahasiswa</a>
                 <br><br>
                 <table class="table table-bordered table-striped table-hover text-center bg-info">
